ec2d86d6f95f6486b5b17ae9feccf2becf073ace
huangwei <huangwei@b.360.cn>
2017-06-26 18:10:13 +0800

    init selectMenu

--- src/components/SelectMenu.vue
+++ src/components/SelectMenu.vue
@@ -3,28 +3,44 @@
     <div class="uk-width-1-1">
       <p class="p-title">Month</p>
       <div class="uk-width-1-1 uk-flex uk-flex-wrap">
-        <button class="uk-button" v-for="op in monthOpts">{{op}}</button>
+        <button :class="{'uk-button-primary': op === month}"
+                class="uk-button"
+                v-for="op in monthOpts"
+                @click="switchMonth(op)">{{op}}
+        </button>
       </div>
     </div>
     <hr>
     <div class="uk-width-1-1">
       <p class="p-title">Chemical</p>
       <div class="uk-width-1-1 uk-flex uk-flex-wrap">
-        <button class="uk-button" v-for="op in chemicalOpts"> {{op}}</button>
+        <button :class="{'uk-button-primary': op === chemical}"
+                class="uk-button"
+                v-for="op in chemicalOpts"
+                @click="switchChemical(op)"> {{op}}
+        </button>
       </div>
     </div>
     <hr>
     <div class="uk-width-1-1">
       <p class="p-title">Sensor</p>
       <div class="uk-width-1-1 uk-flex uk-flex-wrap">
-        <button class="uk-button" v-for="op in sensorOpts"> {{op}}</button>
+        <button v-for="op in sensorOpts"
+                class="uk-button"
+                :class="{'uk-button-primary': op === sensor}"
+                @click="switchSensor(op)"> {{op}}
+        </button>
       </div>
     </div>
     <hr>
     <div class="uk-width-1-1">
       <p class="p-title">Factory</p>
       <div class="uk-width-1-1 uk-flex uk-flex-wrap">
-        <button class="uk-button" v-for="op in factoryOpts"> {{op}}</button>
+        <button v-for="op in factoryOpts"
+                class="uk-button"
+                :class="{'uk-button-primary': op === factory}"
+                @click="switchFactory(op)"> {{op}}
+        </button>
       </div>
     </div>
   </div>
@@ -42,7 +58,25 @@
         factoryOpts: [ 'F1', 'F2', 'F3', 'F4' ],
         chemicalOpts: [ 'C1', 'C2', 'C3', 'C4' ],
         sensorOpts: [ 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9' ],
-        correlationOpts: ['Pearson', 'Spearman']
+        correlationOpts: [ 'Pearson', 'Spearman' ],
+        month: 'M12',
+        chemical: 'C2',
+        factory: 'F4',
+        sensor: 'S3'
+      }
+    },
+    methods: {
+      switchMonth (m) {
+        this.month = m
+      },
+      switchChemical (c) {
+        this.chemical = c
+      },
+      switchSensor (s) {
+        this.sensor = s
+      },
+      switchFactory (f) {
+        this.factory = f
       }
     }
   }
@@ -53,12 +87,14 @@
     border-left: 3px solid #1fa2d6;
     font-weight: bold;
   }
+
   .top {
     padding: 10px;
     button {
       margin-left: 2px;
     }
   }
+
   .bottom {
     margin-top: 10px;
     padding: 10px;
